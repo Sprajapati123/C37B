c7ce4b72a954b67953857c83f1b5929731de3236
Sprajapati123 <sandisprajapati52@gmail.com>
2025-11-26 10:52:33 +0545

    navigation

--- app/src/main/java/com/example/c37b/DashboardActivity.kt
+++ app/src/main/java/com/example/c37b/DashboardActivity.kt
@@ -1,15 +1,19 @@
 package com.example.c37b
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.c37b.ui.theme.C37BTheme
 
@@ -18,30 +22,23 @@ class DashboardActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            C37BTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            DashboardBody()
         }
     }
 }
-
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun DashboardBody(){
+    val context = LocalContext.current
+    val activity = context as Activity
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    C37BTheme {
-        Greeting2("Android")
+    val email = activity.intent.getStringExtra("email")
+    val password = activity.intent.getStringExtra("password")
+    Scaffold {padding->
+        Column (
+            modifier = Modifier.fillMaxSize().padding(padding)
+        ){
+            Text("Email: $email")
+            Text("Password: $password")
+        }
     }
 }
--- app/src/main/java/com/example/c37b/LoginActivity.kt
+++ app/src/main/java/com/example/c37b/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.c37b
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +20,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -34,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -63,6 +68,9 @@ fun LoginBody() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var visibility by remember { mutableStateOf(false) }
+
+    val context = LocalContext.current
+    val activity = context as Activity
     Scaffold { padding ->
         Column(
             modifier = Modifier
@@ -213,6 +221,28 @@ fun LoginBody() {
                     }
                 }
             )
+
+            Button(onClick = {
+                val intent = Intent(
+                    context, DashboardActivity::class.java
+                )
+                intent.putExtra("email",email)
+                intent.putExtra("password",password)
+                context.startActivity(intent)
+                activity.finish()
+
+            }) {
+                Text("Login")
+            }
+            Text("Don't have an account, Signup", modifier = Modifier.clickable {
+                val intent = Intent(
+                    context,
+                    RegistrationActivity::class.java
+                )
+
+                context.startActivity(intent)
+                activity.finish()
+            })
 
         }
     }
