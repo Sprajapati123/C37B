30411d447cae759b0c7bde857443c58219fb671c
Sprajapati123 <sandisprajapati52@gmail.com>
2025-12-15 11:43:30 +0545

    changes

---
+++ app/src/main/java/com/example/c37b/model/ProductModel.kt
@@ -0,0 +1,21 @@
+package com.example.c37b.model
+
+data class ProductModel(
+    var productId : String = "",
+    var productName : String = "",
+    var price : Double = 0.0,
+    var description : String = "",
+    var categoryId : String = "",
+){
+    fun toMap() : Map<String,Any?>{
+        return mapOf(
+            "productName" to productName,
+            "price" to price,
+            "description" to description,
+            "categoryId" to categoryId,
+        )
+    }
+}
+
+
+
