d40bc2b1f0ef13601c943d2c6ce934da3fd1af28
Sprajapati123 <sandisprajapati52@gmail.com>
2025-12-16 11:45:57 +0545

    get all product

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation("androidx.compose.runtime:runtime-livedata:1.10.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
